496582faff33f74561b926f23bc61d911257b39b
Ronit <pandaronit25@gmail.com>
2024-06-22 12:02:34 +0530

    feat: add forward resolver to goole

---
+++ src/forward-resolver.ts
@@ -0,0 +1,40 @@
+import * as dgram from 'node:dgram';
+import { dnsParser } from './message/parser';
+import { DNSObject } from './message/types';
+
+export async function forwardResolver(
+	request: Buffer,
+	host: string,
+	port: number,
+) {
+	const answer = (await new Promise((resolve, reject) => {
+		const socket = dgram.createSocket('udp4');
+
+		socket.on('message', (data, _rinfo) => {
+			try {
+				const header = dnsParser.header(data);
+				const message = dnsParser.questionAndAnswer(data);
+				const response: DNSObject = {
+					header,
+					questions: message.questions,
+					answers: message.answers,
+				};
+				socket.close();
+				resolve(response);
+			} catch (error) {
+				console.error('Error in forwardResolver:', error);
+				reject(error);
+			}
+		});
+
+		socket.on('error', (err) => {
+			socket.close();
+			reject(err);
+		});
+
+		socket.send(request, port, host);
+	})) as DNSObject;
+
+	return answer;
+}
+
--- src/udp-server.ts
+++ src/udp-server.ts
@@ -1,14 +1,21 @@
 import * as dgram from 'node:dgram';
-import { dnsParser } from './message/parser';
-import { DNSObject, QRIndicator, RECORD_TYPE } from './message/types';
+import { forwardResolver } from './forward-resolver';
 import { DNSBuilder } from './message/builder';
+import { dnsParser } from './message/parser';
+import {
+	Bool,
+	DNSAnswer,
+	DNSHeader,
+	DNSObject,
+	QRIndicator,
+} from './message/types';
 
 const udpSocket: dgram.Socket = dgram.createSocket('udp4');
 udpSocket.bind(2053, '127.0.0.1');
 
 console.log('UDP server is running on port 2053');
 
-udpSocket.on('message', (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
+udpSocket.on('message', async (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
 	try {
 		const reqHeaderPacket = dnsParser.header(data);
 		const { questions: reqQuestionPacket } = dnsParser.questionAndAnswer(data);
@@ -31,30 +38,67 @@ udpSocket.on('message', (data: Buffer, remoteAddr: dgram.RemoteInfo) => {
 			throw new Error('QR bit must be 0 to be considered as a valid query');
 		}
 
+		const header: DNSHeader = {
+			ID: reqHeaderPacket.ID,
+			QR: QRIndicator.RESPONSE,
+			OPCODE: reqHeaderPacket.OPCODE,
+			AA: Bool.FALSE,
+			TC: Bool.FALSE,
+			RD: reqHeaderPacket.RD,
+			RA: Bool.TRUE,
+			Z: 0,
+			RCODE: reqHeaderPacket.OPCODE === 0 ? 0 : 4,
+			QDCOUNT: reqQuestionPacket.length,
+			ANCOUNT: reqQuestionPacket.length,
+			NSCOUNT: 0,
+			ARCOUNT: 0,
+		};
+
+		const dnsAnsObjectsFromForwardingServer: DNSAnswer[] = [];
+
+		const forwardingAddress = '8.8.8.8';
+		const forwardingPort = 53;
+
+		for (const question of reqQuestionPacket) {
+			const forwardingRequestObject: DNSObject = {
+				header: {
+					...header,
+					QR: QRIndicator.QUERY,
+					QDCOUNT: 1,
+					ANCOUNT: 0,
+				},
+				questions: [question],
+			};
+			const forwardingRequestBuffer = new DNSBuilder(
+				forwardingRequestObject,
+			).toBuffer();
+
+			const dnsObject = await forwardResolver(
+				forwardingRequestBuffer,
+				forwardingAddress,
+				+forwardingPort,
+			);
+
+			console.log(`Received response: ${JSON.stringify(dnsObject, null, 2)}`);
+
+			if (dnsObject.answers) {
+				dnsAnsObjectsFromForwardingServer.push(...dnsObject.answers);
+			}
+		}
+
 		const responseObject: DNSObject = {
 			header: {
-				...reqHeaderPacket,
-				QR: QRIndicator.RESPONSE,
-				QDCOUNT: reqHeaderPacket.QDCOUNT,
-				ANCOUNT: reqQuestionPacket.length,
+				...header,
+				ANCOUNT: dnsAnsObjectsFromForwardingServer.length,
 			},
 			questions: reqQuestionPacket,
-			answers: reqQuestionPacket.map((question) => {
-				return {
-					NAME: question.NAME,
-					CLASS: 1,
-					TTL: 300,
-					TYPE: RECORD_TYPE.A,
-					RDLENGTH: 4,
-					RDATA: Buffer.from([192, 168, 1, 1]),
-				};
-			}),
+			answers: dnsAnsObjectsFromForwardingServer,
 		};
 
 		const dnsBuilder = new DNSBuilder(responseObject);
 		const response = dnsBuilder.toBuffer();
 
-		console.log(`Sending response to ${remoteAddr.address}:${remoteAddr.port}`);
+		console.log(`Sending response: ${JSON.stringify(responseObject, null, 2)}`);
 
 		udpSocket.send(response, remoteAddr.port, remoteAddr.address);
 	} catch (e) {
