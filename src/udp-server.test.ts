d6e202d180543f8c938e627103f02e58b0bdcc05
Ronit <pandaronit25@gmail.com>
2024-06-29 11:51:24 +0530

    ci: add more test for NS records

--- src/udp-server.test.ts
+++ src/udp-server.test.ts
@@ -9,6 +9,7 @@ import {
 } from './message/types';
 import { DNSBuilder } from './message/builder';
 import { dnsParser } from './message/parser';
+import { decodeRDATA } from './utils';
 
 const udpSocket = dgram.createSocket('udp4');
 const TEST_PORT = 2053;
@@ -262,5 +263,75 @@ test('invalid domain should give an NXDOMAIN rcode in header', async () => {
 
 	// Wait for the response promise to resolve or timeout after a certain period
 	await responsePromise;
-}, 10000);
+});
+test('valid DNS request with NS record', async () => {
+	const dnsRequestObject: DNSObject = {
+		header: {
+			ID: 1234,
+			QR: QRIndicator.QUERY,
+			OPCODE: OPCODE.QUERY,
+			AA: Bool.FALSE,
+			TC: Bool.FALSE,
+			RD: Bool.TRUE,
+			RA: Bool.FALSE,
+			Z: 0,
+			RCODE: RCode.NOERROR,
+			QDCOUNT: 1,
+			ANCOUNT: 0,
+			NSCOUNT: 0,
+			ARCOUNT: 0,
+		},
+		questions: [
+			{
+				NAME: 'ronit.dev',
+				TYPE: RECORD_TYPE.NS,
+				CLASS: 1,
+			},
+		],
+	};
+
+	const dnsBuilder = new DNSBuilder(dnsRequestObject);
+	const requestBuffer = dnsBuilder.toBuffer();
+
+	// Setup a promise to wait for the UDP response
+	const responsePromise = new Promise<void>((resolve, reject) => {
+		udpSocket.send(requestBuffer, TEST_PORT, TEST_HOST, (err) => {
+			if (err) {
+				reject(err);
+			}
+		});
+
+		udpSocket.on('message', (data: Buffer) => {
+			try {
+				const { answers } = dnsParser.questionAndAnswer(data);
+
+				expect(answers).toBeDefined();
+				expect(answers.length).toEqual(2);
+
+				expect(answers[0]?.NAME).toEqual(dnsRequestObject.questions[0]?.NAME);
+				expect(answers[0]?.CLASS).toEqual(1);
+				expect(answers[0]?.TYPE).toEqual(RECORD_TYPE.NS);
+				expect(answers[0]?.RDLENGTH).toEqual(24);
+				expect(answers[0]?.RDATA).toBeDefined();
+				expect(decodeRDATA(answers[0]?.RDATA as Buffer)).toEqual(
+					'greg.ns.cloudflare.com',
+				);
+
+				expect(answers[1]?.NAME).toEqual(dnsRequestObject.questions[0]?.NAME);
+				expect(answers[1]?.CLASS).toEqual(1);
+				expect(answers[1]?.TYPE).toEqual(RECORD_TYPE.NS);
+				expect(answers[1]?.RDLENGTH).toEqual(8);
+				expect(answers[1]?.RDATA).toBeDefined();
+				expect(decodeRDATA(answers[1]?.RDATA as Buffer)).toEqual('khloe'); // this is due to compression in the response
+
+				resolve(); // Resolve the promise when all assertions pass
+			} catch (error) {
+				reject(error); // Reject with error if assertions fail
+			}
+		});
+	});
+
+	// Wait for the response promise to resolve or timeout after a certain period
+	await responsePromise;
+});
 
